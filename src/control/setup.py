261c82d7dcf23b309ff8e6f4bed0145324bece19
ArmaanM77 <armaanmahajanbg@gmail.com>
2025-11-08 12:52:47 +0530

    Link Controls to path

--- src/control/setup.py
+++ src/control/setup.py
@@ -21,7 +21,9 @@ setup(
     entry_points={
         'console_scripts': [
             'run_control = control.control_loop:main',
-            'test_control = control.test_control:main'
+            'test_control = control.test_control:main',
+            'skidpad_path = control.path_planner_skidpad:main',
+            'controls_final = control.controls_final:main',
         ],
     },
 )
